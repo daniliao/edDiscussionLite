5c8d8f347e37d70aa12924538f4b2fa69856fb2f
Daniel Liao <danielliao0426@gmail.com>
2025-03-14 21:33:57 -0700

    add all hw3 JUnit tests

--- FoundCode copy/src/application/viewModel/DiscussionPageViewModel.java
+++ FoundCode copy/src/application/viewModel/DiscussionPageViewModel.java
@@ -8,6 +8,21 @@ import javafx.collections.transformation.FilteredList;
 
 import java.util.*;
 
+/**
+ * <p> Title: Discussion Page View Model </p>
+ * 
+ * <p> Description: The view model for DiscussionPageView that 
+ * process data, interacts with models and send back to view
+ * The code detailed design is based on map, list, and filter
+ * </p>
+ * 
+ * <p> Copyright: Daniel Liao © 2025 </p>
+	 * 
+	 * @author Daniel Liao
+	 * 
+	 * @version 0.00		2025-03-14	Initial baseline 
+ */
+
 public class DiscussionPageViewModel {
     //Lists to store questions, which is observable for view updates
     private final ObservableList<Question> questionInList = FXCollections.observableArrayList();
@@ -23,7 +38,15 @@ public class DiscussionPageViewModel {
         return questionAndAnswer;
     }
 
+    
     // User story 1 and 5: Students to ask or update question
+    /**********
+	 * Creates or updates a question.
+	 * 
+	 * @param text 			The text of the question.
+	 * @param newText 		Flag indicating whether this is a new question or an update.
+	 * @param selected 		The question to update, if applicable.
+	 */
     public void createOrUpdateQuestion(String text, boolean newText, Question selected) {
         if (newText) {
             Question q = new Question(text);
@@ -36,6 +59,14 @@ public class DiscussionPageViewModel {
     }
 
     // User story 2 and 5: Students to propose or update answer
+    /**********
+	 * Creates or updates a answer
+	 * 
+	 * @param text 				The text of the answer.
+	 * @param newText 			Flag indicating whether this is a new answer or an update.
+	 * @param selected			Question The question to which the answer belongs.
+	 * @param selectedAnswer 	The answer to update, if applicable.
+	 */
     public void createOrUpdateAnswer(String text, boolean newText, Question selectedQuestion, Answer selectedAnswer) {
         if (text.isEmpty() || selectedQuestion == null) return;
 
@@ -47,6 +78,11 @@ public class DiscussionPageViewModel {
         }
     }
 
+    /**********
+	 * Delete a question
+	 * 
+	 * @param selected 		Question to delete.
+	 */
     public void deleteQuestion(Question selected) {
         if (selected != null) {
             questionInList.remove(selected);
@@ -54,6 +90,12 @@ public class DiscussionPageViewModel {
         }
     }
 
+    /**********
+	 * Delete an answer
+	 * 
+	 * @param selectedQuestion 	The question that contains the answer to delete.
+	 * @param selectedAnswer 	The answer to delete.
+	 */
     public void deleteAnswer(Question selectedQuestion, Answer selectedAnswer) {
         if (selectedQuestion == null || selectedAnswer == null) return;
 
@@ -72,6 +114,12 @@ public class DiscussionPageViewModel {
         }
     }
 
+    /**********
+	 * Delete an nested reply
+	 * 
+	 * @param parentAnswer 		The answer that contains the reply to delete.
+	 * @param replyToDelete 	The reply to delete.
+	 */
     private boolean deleteNestedReply(Answer parentAnswer, Answer replyToDelete) {
         if (parentAnswer.getReply().contains(replyToDelete)) {
             parentAnswer.getReply().remove(replyToDelete);
@@ -88,6 +136,11 @@ public class DiscussionPageViewModel {
     }
 
     // User story 6: Students that posted the question to announce that a specific answer addressed the issue that prompted the initial question.
+    /**********
+	 * Marks a question as solved.
+	 * 
+	 * @param selected 		The question to mark as solved.
+	 */
     public void solvedQuestion(Question selected) {
         if (selected != null) {
             selected.markAsSolved();
@@ -96,6 +149,14 @@ public class DiscussionPageViewModel {
     }
 
     // User story 4: Students to ask for or suggest clarifications.
+    /**********
+	 * Creates or updates a reply to an existing answer.
+     * 
+     * @param text 				The text of the reply.
+     * @param newText 			Flag indicating whether this is a new reply or an update.
+     * @param selectedAnswer 	The answer to which the reply belongs.
+     * @param selectedReply 	The reply to update, if applicable.
+	 */
     public void createOrUpdateReply(String text, boolean newText, Answer selectedAnswer, Answer selectedReply) {
         if (text.isEmpty() || selectedAnswer == null) return;
 
@@ -109,13 +170,25 @@ public class DiscussionPageViewModel {
 
     // Class ListView, Listview (Javafx 8). (2015). https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/ListView.html (accessed February 10, 2025).
     // Release: Javafx 2.2, Using JavaFX UI Controls: List View | JavaFX 2 Tutorials and Documentation. (2013). https://docs.oracle.com/javafx/2/ui_controls/list-view.htm (accessed February 10, 2025).
- 
+    
+    /**********
+	 * Searches for questions and answers that match the search query.
+	 * 
+	 * @param searchQuery 	The query text to search for.
+	 * @return A filtered list of questions matching the search query.
+	 */
     public FilteredList<Question> initiatedGlobalSearch(String searchQuery) {
         return questionInList.filtered(question ->
             question.getQuestionFromUser().toLowerCase().contains(searchQuery.toLowerCase()) || questionAndAnswer.get(question).stream().anyMatch(answer -> answer.getAnswerFromUser().toLowerCase().contains(searchQuery.toLowerCase()) || matchReply(answer, searchQuery))
         );
     }
 
+    /**********
+	 * Searches for questions and answers that match the search query.
+	 * 
+	 * @param searchQuery 	The text to search for.
+	 * @return A boolean variable indicating matching question, answer, or reply.
+	 */
     private boolean matchReply(Answer answer, String searchQuery) {
         return answer.getReply().stream().anyMatch(reply ->
             reply.getAnswerFromUser().toLowerCase().contains(searchQuery.toLowerCase()) || matchReply(reply, searchQuery)
--- FoundCode copy/src/test/DiscussionPageTests.java
+++ FoundCode copy/src/test/DiscussionPageTests.java
@@ -2,65 +2,528 @@ package test;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import application.model.Answer;
 import application.model.Question;
 import application.viewModel.DiscussionPageViewModel;
-import javafx.scene.control.TextField;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 
 
 import org.junit.Test;
 
+
+/* JavaDoc in next section!
+ * Useful doc for my team:
+ * 
+ * Get Answer & Question objects:
+ * ------------------------------------
+ * 
+ * Get Question: vm.getQuestionInList().get(0);
+ * 
+ * Get Answer: vm.getQuestionAndAnswer().get(question).get(0);
+ * 
+ * Get Reply: vm.getQuestionAndAnswer().get(question).get(0).getReply().get(0);
+ * 
+ * ------------------------------------
+ * ------------------------------------
+ * 
+ * Get Answer & Question in String:
+ * 
+ * Get question: vm.getQuestionInList().get(0).getQuestionFromUser();
+ * 
+ * Get answer: vm.getQuestionAndAnswer().get(question).get(0).getAnswerFromUser();
+ * 
+ * Get reply: vm.getQuestionAndAnswer().get(question).get(0).getReply().get(0).getAnswerFromUser();
+ * 
+ * 
+ * Code walkthrough for get answer in String: 
+ * 			   vm.getQuestionAndAnswer()         .get(question)    .get(0)          .getAnswerFromUser();
+		       In the getQuestionAndAnswer map,  first question,   first answer,    retrieve answer
+   
+   To check FilteredList<Question> aList, you need to use and add question into this first: ObservableList<Question> questions = FXCollections.observableArrayList();
+		       
+		       
+ */
+
+
+/**
+ * <p> Title: DiscussionPageTests </p>
+ * 
+ * <p> Description: Test class for verifying the behavior of the methods in the 
+ * {@link DiscussionPageViewModel} class.
+ * </p>
+ * 
+ * <p> Copyright: Daniel Liao © 2025 </p>
+	 * 
+	 * @author Daniel Liao
+	 * 
+	 * @version 0.00		2025-03-14	Initial baseline 
+ */
+
 public class DiscussionPageTests {
 
+	/**********
+	 * This method tests CreateOrUpdateQuestion in viewModel by creating a new question 
+	 * in the discussion page view model.
+     * 
+     * It verifies that a question is added to the list by checking its content
+     * is as expected.
+     * 
+	 */
 	@Test
 	public void testCreateOrUpdateQuestion_create() {
 		DiscussionPageViewModel vm = new DiscussionPageViewModel();
-		
+
 		vm.createOrUpdateQuestion("Life?", true, null);
-		
-	    assertFalse(vm.getQuestionInList().isEmpty());
+
+		assertFalse(vm.getQuestionInList().isEmpty());
 		assertEquals("Life?", vm.getQuestionInList().get(0).getQuestionFromUser());
-		
+
 	}
-	
+
+	/**********
+	 * This method tests the update functionality of an existing question.
+     * 
+     * It verifies that a question can be updated and that the list storing it is 
+     * affected by the changes.
+     * 
+	 */
 	@Test
 	public void testCreateOrUpdateQuestion_update() {
 		DiscussionPageViewModel vm = new DiscussionPageViewModel();
-		
-		//create question first
+
+		//create a question first
 		vm.createOrUpdateQuestion("old", true, null);
 		assertFalse(vm.getQuestionInList().isEmpty());
-		
+
 		//update, by getting question created
 		vm.createOrUpdateQuestion("new", false, vm.getQuestionInList().get(0));
-		
-	    
+
+
 		assertEquals("new", vm.getQuestionInList().get(0).getQuestionFromUser());
-		
+
 	}
 	
-//	public void createOrUpdateAnswer(String text, boolean newText, Question selectedQuestion, Answer selectedAnswer) {
-//  if (text.isEmpty() || selectedQuestion == null) return;
-//
-//  if (newText) {
-//      Answer a = new Answer(text);
-//      questionAndAnswer.get(selectedQuestion).add(a);
-//  } else if (selectedAnswer != null) {
-//      selectedAnswer.setanswerFromInput(text);
-//  }
-//}
-	
+	/**********
+	 * This method tests condition when a question is not selected for update.
+     * 
+     * It verifies that the existing question remains unchanged when no
+     * question is selected for update.
+     * 
+	 */
 	@Test
-	public void createOrUpdateAnswer_create() {
+	public void testCreateOrUpdateQuestion_null() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create a question first
+		vm.createOrUpdateQuestion("old", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		//did not select question to update
+		vm.createOrUpdateQuestion("new", false, null);
+
+
+		assertEquals("old", vm.getQuestionInList().get(0).getQuestionFromUser());
+
+	}
+	/**********
+	 * This method tests the creation of an answer
+     * 
+     * It verifies that an answer is correctly liked with a question.
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateAnswer_create() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		assertEquals("is life", vm.getQuestionAndAnswer().get(question).get(0).getAnswerFromUser());
+	}
+
+	/**********
+	 * This method test the update functionality of an existing answer.
+     * 
+     * It verifies that an answer can be updated and the updated answer
+     * is correctly changed in the list.
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateAnswer_update() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		// get the first question
+		Question question = vm.getQuestionInList().get(0);
+		vm.createOrUpdateAnswer("is life", true, question, null);
 		
+		vm.createOrUpdateAnswer("updated life", false, question, vm.getQuestionAndAnswer().get(question).get(0));
+
+		// Code Translation:
+		// 	vm.getQuestionAndAnswer()		 .get(question)	 .get(0)		  .getAnswerFromUser()
+		// 	In the getQuestionAndAnswer map, first question, first answer,    retrieve answer
+		assertEquals("updated life", vm.getQuestionAndAnswer().get(question).get(0).getAnswerFromUser());
 	}
 	
+	/**********
+	 * This method test the condition when an answer is not selected for update.
+     * 
+     * It verifies that the answer is unchanged when no answer is selected.
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateAnswer_null() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
 
+		//create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		// get the first question
+		Question question = vm.getQuestionInList().get(0);
+		vm.createOrUpdateAnswer("is life", true, question, null);
+		
+		vm.createOrUpdateAnswer("updated life", false, question, null);
+
+		// Code Translation:
+		// 	vm.getQuestionAndAnswer()		 .get(question)	 .get(0)		  .getAnswerFromUser()
+		// 	In the getQuestionAndAnswer map, first question, first answer,    retrieve answer
+		assertEquals("is life", vm.getQuestionAndAnswer().get(question).get(0).getAnswerFromUser());
+	}
+
+	/**********
+	 * This method test the deletion of a question.
+     * 
+     * It verifies that a question is removed from the list after delete method is called.
+     * 
+	 */
+	@Test
+	public void testDeleteQuestion() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create question first
+		vm.createOrUpdateQuestion("neww", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		// get the first question
+		Question question = vm.getQuestionInList().get(0);
+		vm.deleteQuestion(question);
+
+		assertFalse(vm.getQuestionInList().contains(question));
+	}
 	
+	/**********
+	 * This method test the deletion of a top-level answer.
+     * 
+     * It verifies that an answer is removed from the list after delete method is called.
+     * 
+	 */
+	@Test
+	public void testDeleteAnswer_topLevelAnswer() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
 
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		vm.deleteAnswer(question, ans);
+
+		assertFalse(vm.getQuestionAndAnswer().get(question).contains(ans));
+	}
+
+	/**********
+	 * This method tests deleting a reply
+     * 
+	 */
+	@Test
+	public void testDeleteAnswer_replies() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeeeply", true, ans, null);
+
+		Answer replllly = ans.getReply().get(0);
+
+		//delete reply
+		vm.deleteAnswer(question, replllly);
+
+		assertFalse(ans.getReply().contains(replllly));
+	}
+
+	/**********
+	 * This method tests deleting a nested reply from an answer
+     * 
+	 */
+	@Test
+	public void testDeleteNestedReply() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeeeply", true, ans, null);
+
+		Answer replllly = ans.getReply().get(0);
+
+		//delete reply
+		vm.deleteAnswer(question, replllly);
+
+		assertFalse(ans.getReply().contains(replllly));
+	}
+
+	/**********
+	 * This method tests marking question as solved
+     * 
+	 */
+	@Test
+	public void testSolvedQuestion() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create question first
+		vm.createOrUpdateQuestion("quesss", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+		vm.solvedQuestion(question);
+
+		assertEquals("Question (solved): quesss", question.toString());
+		assertTrue("marked as solved", question.isSolved());
+	}
+
+	/**********
+	 * This method tests the condition when user did not select question but 
+	 * press the solved button
+     * 
+	 */
+	@Test
+	public void testSolvedQuestion_noSelected() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//create question first
+		vm.createOrUpdateQuestion("quesss", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+		vm.solvedQuestion(null);
+
+		assertEquals("quesss", question.getQuestionFromUser());
+		assertFalse(question.isSolved());
+	}
+	
+	/**********
+	 * This method tests creating a reply
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateReply_create() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		vm.createOrUpdateReply("reeeeply", true, ans, null);
+
+		assertEquals("reeeeply", ans.getReply().get(0).getAnswerFromUser());
+
+	}
+
+	/**********
+	 * This method tests updating a reply
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateReply_update() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeeeply", true, ans, null);
+
+		Answer replllly = ans.getReply().get(0);
+
+		vm.createOrUpdateReply("newply", false, ans, replllly);
+
+		assertEquals("newply", ans.getReply().get(0).getAnswerFromUser());
+
+	}
+
+	/**********
+	 * This method tests not selecting a reply to update
+     * 
+	 */
+	@Test
+	public void testCreateOrUpdateReply_noSelectedReply() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+
+		//	create question first
+		vm.createOrUpdateQuestion("question", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeeeply", true, ans, null);
+
+		// no selected reply
+		vm.createOrUpdateReply("newply", false, ans, null);
+
+		assertEquals("reeeeply", ans.getReply().get(0).getAnswerFromUser());
+
+	}
+
+	/**********
+	 * This method tests searching functionality when the search query 
+	 * exists in question, answer, and reply
+     * 
+	 */
+	@Test
+	public void testInitiatedGlobalSearch_foundInAllLevel() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+		
+		ObservableList<Question> questions = FXCollections.observableArrayList();
+		
+		// create question first
+		vm.createOrUpdateQuestion("questioi7on", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is oi7life", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeeoi7eply", true, ans, null);
+		
+		questions.add(question);
+		
+		FilteredList<Question> shouldInclude = vm.initiatedGlobalSearch("oi7");
+		
+		assertEquals("questioi7on", shouldInclude.get(0).getQuestionFromUser());
+		assertTrue(vm.getQuestionAndAnswer().get(question)
+                .stream().anyMatch(a -> a.getAnswerFromUser().contains("oi7")));
+
+        assertTrue(ans.getReply()
+                .stream().anyMatch(r -> r.getAnswerFromUser().contains("oi7")));
+	}
+	
+	/**********
+	 * This method tests the condition when the search query can not be found
+     * 
+	 */
+	@Test
+	public void testInitiatedGlobalSearch_notFound() {
+		DiscussionPageViewModel vm = new DiscussionPageViewModel();
+		
+		ObservableList<Question> questions = FXCollections.observableArrayList();
+		
+		// create question first
+		vm.createOrUpdateQuestion("questio7on", true, null);
+		assertFalse(vm.getQuestionInList().isEmpty());
+
+		Question question = vm.getQuestionInList().get(0);
+
+		//	create answer first
+		vm.createOrUpdateAnswer("is oilife", true, question, null);
+
+		//get answer
+		Answer ans = vm.getQuestionAndAnswer().get(question).get(0);
+
+		//create reply
+		vm.createOrUpdateReply("reeei7eply", true, ans, null);
+		
+		questions.add(question);
+		
+		FilteredList<Question> shouldInclude = vm.initiatedGlobalSearch("oi7");
+		
+		assertTrue(shouldInclude.isEmpty());
+		assertFalse(vm.getQuestionAndAnswer().get(question)
+                .stream().anyMatch(a -> a.getAnswerFromUser().contains("oi7")));
+
+        assertFalse(ans.getReply()
+                .stream().anyMatch(r -> r.getAnswerFromUser().contains("oi7")));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
